a613cd6638ad24f5baa4e272e562a0cafec72fca
Clinton Pearce <clint.pearce@rocketmail.com>
2020-10-14 15:10:33 -0400

    Add user signups

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,24 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # A D M I N   F U N C T I O N S
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
+
+  # U S E R   M A N A G E M E N T
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
